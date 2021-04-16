37222cc71b9d372a31c49b54d73f07ccc238d315
MAlejandro26 <62815393+MAlejandro26@users.noreply.github.com>
2021-04-16 16:14:49 -0600

    firt commit

---
+++ src/controllers/controller.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+const { parse } = require('pg-connection-string');
+
+const connectionString =  'postgres://hwohblrhffqffc:499d99fac14514e4e31355e40b5f209d6c8d58372d3cd816b44f4d6dfef80282@ec2-3-233-43-103.compute-1.amazonaws.com:5432/dcu5e78f3mavdk';
+
+const config = parse(connectionString);
+
+config.ssl = {
+    rejectUnauthorized: false
+}
+
+const pool = new Pool(config);
+
+const welcome = async (req, res) =>{
+    res.status(200).json({
+        message: 'Ha fallado, ups'
+    })
+}
+
+const getGames = async (req, res) => {
+    const response = await pool.query('Select * from games;');
+    res.status(200).json(response.rows);
+};
+
+module.exports = {
+    welcome,
+    getGames
+};
